f069fcfe176afdd2d0d2cdb913401b5994eb505f
Mona Lisa <sistemas@porter.com.mx>
2024-02-01 12:13:19 -0600

    Subiendo contacto

---
+++ components/Contact/FormContact.php
@@ -0,0 +1,71 @@
+<div id="contact" class="section wb">
+        <div class="container">
+            <div class="section-title text-center">
+                <h3>Ponerse en contacto</h3>
+                <p class="lead"> Permítanos brindarle más detalles sobre el sitio web de oferta especial que desea. Por favor, rellene el siguiente formulario.</p>
+            </div><!-- end title -->
+
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="contact_form">
+                        <div id="message"></div>
+                        <form id="contactform" class="row" action="contact.php" name="contactform" method="post">
+                            <fieldset class="row-fluid">
+                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                    <input type="text" name="first_name" id="first_name" class="form-control" placeholder="Nombre">
+                                </div>
+                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                    <input type="text" name="last_name" id="last_name" class="form-control" placeholder="Apellidos">
+                                </div>
+                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                    <input type="email" name="email" id="email" class="form-control" placeholder="Corre eléctronico">
+                                </div>
+                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                    <input type="text" name="phone" id="phone" class="form-control" placeholder="Télefono">
+                                </div>
+                                <g-12 col-md-12 col-sm-12 col-xs-12">
+                                    <textarea class="form-control" name="comments" id="comments" rows="6" placeholder="Danos más detalles.."></textarea>
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 text-center">
+                                    <button type="submit" value="SEND" id="submit" class="btn btn-light btn-radius btn-brd grd1 btn-block">Enviar mensaje</button>
+                                </div>
+                            </fieldset>
+                        </form>
+                    </div>
+                </div><!-- end col -->
+            </div><!-- end row -->
+			
+			<div class="row">
+				<div class="col-md-offset-1 col-sm-10 col-md-10 col-sm-offset-1 pd-add">
+					<div class="address-item">
+						<div class="address-icon">
+							<i class="icon icon-location2"></i>
+						</div>
+						<h3>Manzanillo</h3>
+						<p>Acambaro #444, Col. Tapeixtles, <br> C.P.: 28876 Manzanillo, Colima, Mx.
+					</div>
+					<div class="address-item">
+						<div class="address-icon">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</div>
+						<h3>Envíanos un correo electrónico</h3>
+						<p>info@mepacsa.com
+					</div>
+					<div class="address-item">
+						<div class="address-icon">
+							<i class="icon icon-headphones"></i>
+						</div>
+						<h3>Llama al</h3>
+						<p>314 33 67374
+							<br>314 33 67289
+                    </p>
+					</div>
+				</div>
+			</div><!-- end row -->
+			<div class="row">
+				<div class="col-md-offset-1 col-sm-10 col-md-10 col-sm-offset-1 pd-add">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.6221873152463!2d-104.27655202574812!3d19.080341951838054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8424d586ee51c197%3A0x7fa8404e17199f7d!2sManiobras%20Estrategicas%20del%20Pac%C3%ADfico%20SA%20DE%20CV!5e0!3m2!1ses-419!2smx!4v1706810959358!5m2!1ses-419!2smx" width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				</div>
+			</div><!-- end row -->
+        </div><!-- end container -->
+    </div><!-- end section -->
