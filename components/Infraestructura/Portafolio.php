aa3c13aa4a7cadc4e205228bca33bf134b04d214
Mona Lisa <sistemas@porter.com.mx>
2024-02-01 12:00:48 -0600

    Subiendo infraestructura

---
+++ components/Infraestructura/Portafolio.php
@@ -0,0 +1,83 @@
+<div id="portfolio" class="section wb">
+        <div class="container">
+            <div class="section-title text-center">
+                <h3>Infraestructura</h3>
+                <p class="lead">Con una superficie de 58,269 m2 dividida en 17,678 m2 de bodegas techadas y 40,591 m2 de patio, Maniobras Estratégicas del Pacifico se localiza a tan solo 2 km. Del puerto de Manzanillo, contando con una excelente ubicación para sus operaciones dentro y fuera del puerto.</p>
+            </div><!-- end title -->
+        </div><!-- end container -->
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <nav class="portfolio-filter text-center">
+                        <ul>
+                            <li><a class="btn btn-dark btn-radius btn-brd active" href="#" data-filter="*"><span class="oi hidden-xs" data-glyph="grid-three-up"></span> Todos</a></li>
+                            <li><a class="btn btn-dark btn-radius btn-brd" data-toggle="tooltip" data-placement="top" title="5" href="#" data-filter=".cat1">Oficinas</a></li>
+                            <li><a class="btn btn-dark btn-radius btn-brd" href="#" data-toggle="tooltip" data-placement="top" title="12" data-filter=".cat2">Patios</a></li>
+                            <li><a class="btn btn-dark btn-radius btn-brd" href="#" data-toggle="tooltip" data-placement="top" title="21" data-filter=".cat3">Seguridad</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+
+            <hr class="invis">
+
+            <div id="da-thumbs" class="da-thumbs portfolio">
+                <div class="post-media pitem item-w1 item-h1 cat1">
+                    <a href="assets/uploads/portfolio_01.jpg" data-rel="prettyPhoto[gal]">
+                        <img src="assets/uploads/portfolio_01.jpg" alt="" class="img-responsive">
+                        <div>
+                            <h3>Acceso <small>Mepacsa</small></h3>
+                            <i class="flaticon-unlink"></i>
+                        </div>
+                    </a>
+                </div>
+                <div class="post-media pitem item-w1 item-h1 cat1">
+                    <a href="assets/uploads/portfolio_02.jpg" data-rel="prettyPhoto[gal]">
+                        <img src="assets/uploads/portfolio_02.jpg" alt="" class="img-responsive">
+                        <div>
+                            <h3>Oficinas  <small>centrales</small></h3>
+                            <i class="flaticon-unlink"></i>
+                        </div>
+                    </a>
+                </div>
+                <div class="post-media pitem item-w1 item-h1 cat2">
+                    <a href="assets/uploads/portfolio_03.jpg" data-rel="prettyPhoto[gal]">
+                        <img src="assets/uploads/portfolio_03.jpg" alt="" class="img-responsive">
+                        <div>
+                            <h3>Patíos de vacíos <small>Carretera Manzanillo-Minatilán 1.2 KM</small></h3>
+                            <i class="flaticon-unlink"></i>
+                        </div>
+                    </a>
+                </div>
+                <div class="post-media pitem item-w1 item-h1 cat3">
+                    <a href="assets/uploads/portfolio_04.jpg" data-rel="prettyPhoto[gal]">
+                        <img src="assets/uploads/portfolio_04.jpg" alt="" class="img-responsive">
+                        <div>
+                            <h3>Vigilancia 24/7 <small>Guardias de seguridad y CCTV</small></h3>
+                            <i class="flaticon-unlink"></i>
+                        </div>
+                    </a>
+                </div>
+                <div class="post-media pitem item-w1 item-h1 cat2">
+                    <a href="assets/uploads/portfolio_05.jpg" data-rel="prettyPhoto[gal]">
+                        <img src="assets/uploads/portfolio_05.jpg" alt="" class="img-responsive">
+                        <div>
+                            <h3>Patio  <small>regulador</small></h3>
+                            <i class="flaticon-unlink"></i>
+                        </div>
+                    </a>
+                </div>
+                <div class="post-media pitem item-w1 item-h1 cat3">
+                    <a href="assets/uploads/portfolio_06.jpg" data-rel="prettyPhoto[gal]">
+                        <img src="assets/uploads/portfolio_06.jpg" alt="" class="img-responsive">
+                        <div>
+                            <h3>2 Basculas electromecánicas <small>Certificadas y verificadas ante EMA</small></h3>
+                            <i class="flaticon-unlink"></i>
+                        </div>
+                    </a>
+                </div>
+               
+            </div><!-- end portfolio -->
+        </div><!-- end container -->
+    </div><!-- end section -->
