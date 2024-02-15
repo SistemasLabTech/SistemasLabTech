658d548e5d1aae4c0b0f0131bbd7a98524f19f1b
Mona Lisa <sistemas@porter.com.mx>
2024-02-15 11:47:57 -0600

    Subiendo cambios necesarios

---
+++ components/Index/Services.php
@@ -0,0 +1,86 @@
+<div id="services" class="parallax section lb">
+        <div class="container">
+            <div class="section-title text-center">
+                <h3>Our Service</h3>
+                <p class="lead">Our Service unlimited solutions to all your business needs. in the installation package we prepare search engine optimization, social media support, we provide corporate identity and graphic design services.</p>
+            </div><!-- end title -->
+
+            <div class="owl-services owl-carousel owl-theme">
+               
+                <!-- end service -->
+
+                <div class="service-widget">
+                    <div class="post-media wow fadeIn">
+                        <a href="assets/uploads/portfolio_04.jpg" data-rel="prettyPhoto[gal]" class="hoverbutton global-radius"><i class="flaticon-unlink"></i></a>
+                        <img src="assets/uploads/portfolio_04.jpg" alt="" class="img-responsive img-rounded">
+                    </div>
+					<div class="service-dit">
+						<h3>BASCULAS </h3>
+						<p>Duis at tellus at dui tincidunt scelerisque nec sed felis. Suspendisse id dolor sed leo rutrum euismod. Nullam vestibulum fermentum erat. It nam auctor. </p>
+					</div>
+                </div>
+                <!-- end service -->
+
+                <div class="service-widget">
+                    <div class="post-media wow fadeIn">
+                        <a href="assets/uploads/service_03.jpg" data-rel="prettyPhoto[gal]" class="hoverbutton global-radius"><i class="flaticon-unlink"></i></a>
+                        <img src="assets/uploads/service_03.jpg" alt="" class="img-responsive img-rounded">
+                    </div>
+					<div class="service-dit">
+						<h3>ALMACÉN FISCAL </h3>
+						<p>Etiam materials ut mollis tellus, vel posuere nulla. Etiam sit amet lacus vitae massa sodales aliquam at eget quam. Integer ultricies et magna quis accumsan.</p>
+					</div>
+                </div>
+                <!-- end service -->
+
+                <div class="service-widget">
+                    <div class="post-media wow fadeIn">
+                        <a href="assets/uploads/service_04.jpg" data-rel="prettyPhoto[gal]" class="hoverbutton global-radius"><i class="flaticon-unlink"></i></a>
+                        <img src="assets/uploads/service_04.jpg" alt="" class="img-responsive img-rounded">
+                    </div>
+					<div class="service-dit">
+						<h3>FERTILIZANTES </h3>
+						<p>Praesent in neque congue sapien lobortis faucibus id eget erat. <br>Pellentesque maximus rutrum felis. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+					</div>
+                </div>
+
+                <div class="service-widget">
+                    <div class="post-media wow fadeIn">
+                        <a href="assets/uploads/service_04.jpg" data-rel="prettyPhoto[gal]" class="hoverbutton global-radius"><i class="flaticon-unlink"></i></a>
+                        <img src="assets/uploads/service_04.jpg" alt="" class="img-responsive img-rounded">
+                    </div>
+					<div class="service-dit">
+						<h3>TRANSPORTES  </h3>
+						<p>Praesent in neque congue sapien lobortis faucibus id eget erat. <br>Pellentesque maximus rutrum felis. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+					</div>
+                </div>
+                <div class="service-widget">
+                    <div class="post-media wow fadeIn">
+                        <a href="assets/uploads/service_04.jpg" data-rel="prettyPhoto[gal]" class="hoverbutton global-radius"><i class="flaticon-unlink"></i></a>
+                        <img src="assets/uploads/service_04.jpg" alt="" class="img-responsive img-rounded">
+                    </div>
+					<div class="service-dit">
+						<h3>MINERAL  </h3>
+						<p>Praesent in neque congue sapien lobortis faucibus id eget erat. <br>Pellentesque maximus rutrum felis. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+					</div>
+                </div>
+                <div class="service-widget">
+                    <div class="post-media wow fadeIn">
+                        <a href="assets/uploads/service_04.jpg" data-rel="prettyPhoto[gal]" class="hoverbutton global-radius"><i class="flaticon-unlink"></i></a>
+                        <img src="assets/uploads/service_04.jpg" alt="" class="img-responsive img-rounded">
+                    </div>
+					<div class="service-dit">
+						<h3>ESPUELA DE FERROCARRIL  </h3>
+						<p>Praesent in neque congue sapien lobortis faucibus id eget erat. <br>Pellentesque maximus rutrum felis. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+					</div>
+                </div>
+                <!-- end service -->
+            </div><!-- end row -->
+
+            <hr class="hr1">
+
+            <div class="text-center">
+                <a data-scroll href="#portfolio" class="btn btn-light btn-radius btn-brd">Ver más detalles</a>
+            </div>
+        </div><!-- end container -->
+    </div>
--- contact.php
+++ contact.php
@@ -4,34 +4,13 @@
     <?php include './static/head.php'; ?>
 </head>
 <body>
-    <?php include './static/loader.php'; ?>
-	<?php include './static/topbar.php'; ?>
-    <header class="header header_style_01">
-        <nav class="megamenu navbar navbar-default">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="index.php"><img src="https://www.mepacsa.com/images/logo-foot.png"   alt="image"></a>
-                </div>
-                <div id="navbar" class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a  href="index.php">Inicio</a></li>
-                        <li><a href="about-us.php">Acerca de</a></li>
-                        <li><a href="services.php">Nuestro servicios</a></li>
-                        <li><a href="portfolio.php">Infraestructura</a></li>
-						<li><a class="active" href="contact.php">Contacto</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
-    <?php include './components/Contact/Banner.php'; ?>
-    <?php include './components/Contact/FormContact.php'; ?>
-    <?php include './static/footer.php'; ?>
+    <?php
+    include './static/loader.php';
+    include './static/topbar.php';
+    include './components/Contact/Menu.php';
+    include './components/Contact/Banner.php';
+    include './components/Contact/FormContact.php';
+    include './static/footer.php';
+    ?>
 </body>
 </html>
